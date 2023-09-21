733d88a2f52db77cbe1990001c2ff937e34078fe
COBRAHALOLEAFSPEACELOVE7 <129617008+COBRAHALOLEAFSPEACELOVE7@users.noreply.github.com>
2023-09-21 16:21:11 -0400

    Add files via upload

---
+++ Website/Website/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿var id = null;
+function Move() {
+    var elem = document.getElementById("Animation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
